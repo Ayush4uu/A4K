b8c517a14a382d39f90926b20972249eb83b00c6
Ayush4uu <spiceee7z@gmail.com>
2024-08-18 13:23:39 +0530

    Update magnific-popup-options.js

--- js/magnific-popup-options.js
+++ js/magnific-popup-options.js
@@ -82,4 +82,12 @@ $(document).ready(function() {
      
      // gallery
      
-     
+     const showPopup = document.querySelector('.show-popup');
+const popupContainer = document.querySelector('.popup-container');
+const closeBtn = document.querySelector('.close-btn');
+showPopup.onclick = () => {
+    popupContainer.classList.add('active');
+}
+closeBtn.onclick = () => {
+    popupContainer.classList.remove('active');
+}
